bebffe7b4ba927e7fd414328763cb0bd9c5e2d51
hello886 <553882335@163.com>
2020-04-17 09:52:55 +0800

    功能ok

--- test01.py
+++ test01.py
@@ -1,16 +1,19 @@
 # coding=utf-8
 # 基于conda env:py35
 # abcabfdbbad
-tmpStr = 'abcabfdbbad'
-lenStr = len(tmpStr)
-df = [[0 for y in range(lenStr)] for x in range(lenStr)]
-for x in range(lenStr):
-    for y in range(x + 1, lenStr):
-        print(str(x), str(y))
-        print(df[x])
-        print(tmpStr[(y - 1 - df[x][y - 1]):y + 1])
-        notFound = int(tmpStr[(y - 1 - df[x][y - 1]):y].find(tmpStr[y]) < 0)
+
+paramStr = 'abcabfdbbad'
+paramStrLen = len(paramStr)
+df = [[0 for y in range(paramStrLen)] for x in range(paramStrLen)]
+for x in range(paramStrLen):
+    for y in range(x + 1, paramStrLen):
+        notFound = int(paramStr[(y - 1 - df[x][y - 1]):y].find(paramStr[y]) < 0)
         df[x][y] = (df[x][y - 1] + 1) if notFound else 0
 
-for i in range(lenStr):
+print('cal matrix:')
+for i in range(paramStrLen):
     print(df[i])
+maxNum = max(map(max, df))
+print('max len：', maxNum)
+print('max str:\n' + str(
+    list([paramStr[x:y] for x in range(paramStrLen) for y in range(x + 1, paramStrLen) if df[x][y] == maxNum])))
