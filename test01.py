6117559ee36385d810497c913128e10eda632995
hello886 <553882335@163.com>
2020-04-17 09:38:04 +0800

    功能ok

--- test01.py
+++ test01.py
@@ -1,14 +1,16 @@
 # coding=utf-8
 # 基于conda env:py35
-
+# abcabfdbbad
 tmpStr = 'abcabfdbbad'
 lenStr = len(tmpStr)
-df = [[1 for y in range(lenStr)] for x in range(lenStr)]
+df = [[0 for y in range(lenStr)] for x in range(lenStr)]
 for x in range(lenStr):
     for y in range(x + 1, lenStr):
         print(str(x), str(y))
-        find = int(tmpStr[y - df[x][y - 1]:y + 1].find(tmpStr[y]) < 0)
-        df[x][y] = (df[x][y - 1] + 1) if find else 1
+        print(df[x])
+        print(tmpStr[(y - 1 - df[x][y - 1]):y + 1])
+        notFound = int(tmpStr[(y - 1 - df[x][y - 1]):y].find(tmpStr[y]) < 0)
+        df[x][y] = (df[x][y - 1] + 1) if notFound else 0
 
 for i in range(lenStr):
     print(df[i])
