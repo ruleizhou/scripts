25a76f07dc60e14f462288c1c013be18cf460c08
hello886 <553882335@163.com>
2020-04-19 21:57:53 +0800

    微调

--- script_pkl2Csv2Center.py
+++ script_pkl2Csv2Center.py
@@ -3,7 +3,7 @@
 
 # 将pkl转为csv格式(方便直接导入数据库等）
 # python xx.py pklPath raname_map
-# python script_pkl2Csv2Center.py ~/下载/dd_price_vp_20190601_20200421.pkl '{"index":"datetime"}'
+# python script_pkl2Csv2Center.py ~/下载/dd_price_vp_20190601_20200421.pkl '{"index":"datetime","volume":"vol"}'
 # 步骤
 # 1,依次取得pkl文件minor_xs轴维度 as df
 # 2,df.reset_index(),df.dropna(),df拼接为all_df
@@ -14,9 +14,6 @@
 import os
 import sys
 import pandas as pd
-
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
 
 # 获取文件路径、文件名、后缀
