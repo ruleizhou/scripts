077767e3ccec84aefdc8b9bea4c4994fa691bea9
yuanjh6 <553882335@163.com>
2020-09-04 22:28:50 +0800

    test

--- cta_machineInfo/cta_machineInfo.py
+++ cta_machineInfo/cta_machineInfo.py
@@ -2,37 +2,17 @@
 ###########################3
 #ubuntu查看穿透监管相关信息
 
-from ctypes import *
 import os
-import chardet
+from ctypes import *
+
 file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'LinuxDataCollect.so')
-
 libtest = cdll.LoadLibrary(file_path)
-
-# testpy = libtest.CTP_GetSystemInfo
-# testpy.argtype = c_char_p
-# testpy.restype = c_int
-# ss = "/systemInfo.sys"
-# yy = 0
-# params = testpy(ss,yy)
-# print params
-
-#_Z28CTP_GetSystemInfoUnAesEncodePcRi
-#_Z17CTP_GetSystemInfoPcRi
-#_Z21CTP_GetRealSystemInfoPcRi
-func = getattr(libtest, '_Z21CTP_GetRealSystemInfoPcRi')
-# func.argtypes = [c_char, c_int]
-# func.restype = c_int
-a=create_string_buffer(264)
-b=c_int()
-print (func(a, byref(b)))
-print (a.value)
-print (b)
-# ret = chardet.detect(a.value)
-# print(ret)
-
-# print(a.value.encode('raw_unicode_escape'))
-# print(a.value.encode('unicode_escape'))
-# print(a.value.encode('utf-8'))
-# print(a.value.encode('gbk'))
-# print(a.value.encode('gb2312'))
+# '_Z28CTP_GetSystemInfoUnAesEncodePcRi', '_Z17CTP_GetSystemInfoPcRi',
+func_str = '_Z21CTP_GetRealSystemInfoPcRi'
+func = getattr(libtest, func_str)
+a = create_string_buffer(264)
+b = c_int()
+print('func_str %s' % func_str)
+print(func(a, byref(b)))
+print(a.value)
+print(b)
